da9d7a8467d5f02ad8b2446f88a5ac80c041efa6
DanKaptijn <33229007+DanKaptijn@users.noreply.github.com>
2021-07-07 14:59:47 +0200

    Update train.py

--- yeastcells/train.py
+++ yeastcells/train.py
@@ -58,6 +58,7 @@ def validate_labels(labels, path):
                 for ann in label['annotations']
                 if len(ann['segmentation'][0]) >= 6
             ]
+            print(label['annotations'])
             assert len(label['annotations']) > 0
             label['file_name'] = path + '/' + label['file_name']
 
